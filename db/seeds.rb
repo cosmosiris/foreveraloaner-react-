dd44fad3741bc0057d6a43e5fe0dc8e422bc5796
Caroline & Shannon Nolan <pair+carolyoon.shannonnolan17@devbootcamp.com>
2017-11-11 17:39:38 -0800

    Rewrite categories in alphabetical order

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Review.destroy_all
+Category.destroy_all
 
 borrower = User.create(first_name: "britt", last_name: "nevins", email:"britt@nevins.com", password: "asdfasdfasdf")
 lender = User.create(first_name: "Olivia", last_name: "Noland", email:"olivia@noland.com", password: "asdfasdfasdf")
@@ -14,4 +15,8 @@ lender = User.create(first_name: "Olivia", last_name: "Noland", email:"olivia@no
 
 Review.create(rating: 3, body: "great", reviewer: borrower, reviewee: lender, role:"borrower")
 
+categories = ["Accessories", "Baby & Kid Products", "Beauty Products", "Books", "Business & Office Supplies", "Clothing", "DIY Supplies", "Electronics", "Farm & Garden", "Fitness & Sporting Equipment", "Furniture", "Games", "Household Items & Appliances", "Kitchen Appliances", "Luggage & Travel Accessories", "Miscellaneous", "Musical Instruments", "Outdoor Equipment", "Party Supplies", "Tools", "Trailers", "Vehicle & Automotive Parts", "VHS & DVD" ]
 
+categories.each do |category|
+  Category.create(name: category)
+end
