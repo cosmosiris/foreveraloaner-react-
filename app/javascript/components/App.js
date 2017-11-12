5e734c7d0d233f757f9a01caf03db0bb7a70bdfb
Caroline & Shannon Nolan <pair+carolyoon.shannonnolan17@devbootcamp.com>
2017-11-12 14:44:42 -0800

    Create get method to render categories index on homepage with axios

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,29 +1,40 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Categories from './Categories'
-import SearchForm from './SearchForm'
-
-
+import React from "react";
+import PropTypes from "prop-types";
+import Categories from './Categories';
+import SearchForm from './SearchForm';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(){
     super();
 
     this.state = {
-      posts: {},
-      reviews: {},
-      transactions: {},
-      tags: {}
+      categories:[],
+      posts: [],
+      reviews: [],
+      transactions: [],
+      tags: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/categories')
+    .then(res => {
+      console.log(res)
+      const categories = res.data.categories.map( (category) =>
+        ({id: category.id, name: category.name }))
+      this.setState( {categories} )
+    })
   }
 
   render () {
     return(
     	<div>
-            <SearchForm />
-
-            <Categories />
-        </div>
+        <SearchForm />
+        {
+          this.state.categories.map(category => <Categories key={category.id} name={category.name} />)
+        }
+      </div>
     )
   }
 }
--- app/javascript/components/Categories.js
+++ app/javascript/components/Categories.js
@@ -2,10 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class Categories extends React.Component {
+
   render () {
     return(
     	<div>
-    		<p>Categories Component, will list all the categories</p>
+        <p>{this.props.name}</p>
     	</div>
     )
   }
