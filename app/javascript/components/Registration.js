021875d44b1da3362a12ab0ece2532625025a666
ikn02010 <ikn02010@gmail.com>
2017-11-12 16:29:38 -0800

    Download axios

--- app/javascript/components/Registration.js
+++ app/javascript/components/Registration.js
@@ -2,14 +2,50 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class Registration extends React.Component {
-  render () {
-    return(
-    	<div className="Registration" style={{height: '200px', backgroundColor: 'honeydew'}}>
-    		<h2>Registration COMPONENT</h2>
-    		
-    	</div>
-    )
-  }
+    constructor(){
+        super();
+        this.state={
+            firstName: 'My realname',
+            lastName: '',
+            email: '',
+            password: ''
+        };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.submitFormData = this.submitFormData.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({firstName: event.target.firstName});
+    }
+
+    handleSubmit(event){
+        alert('a name was submitted: ' + this.state.firstName);
+        event.preventDefault();
+    }
+
+    submitFormData(){
+        axios.post('/user', {
+        firstName: this.state.firstName
+        }).then(function(response){
+            console.log(response);
+        }).catch(function(error){
+            console.log(error)
+        })
+    }
+
+    render () {
+        return(
+        	<div className="Registration" style={{height: '200px', backgroundColor: 'honeydew'}}>
+        		<h2>Registration COMPONENT</h2>
+                <form onSubmit={this.submitFormData()}>
+                    <input type="text" value={this.state.firstName} onChange={this.handleChange} placeholder="first name"/>
+                    <input type="submit"/>
+                </form>
+                <p>{this.state.firstName}</p>
+        	</div>
+        )
+      }
 }
 
 export default Registration
