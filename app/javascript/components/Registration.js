b35ea4d7b97210ce64d00f1aa98aa290bd9fe2e1
ikn02010 <ikn02010@gmail.com>
2017-11-12 20:10:10 -0800

    Connect registration component to users_controller. Configure routes to match

--- app/javascript/components/Registration.js
+++ app/javascript/components/Registration.js
@@ -1,48 +1,77 @@
 import React from "react"
 import PropTypes from "prop-types"
+import axios from 'axios'
 
 class Registration extends React.Component {
     constructor(){
         super();
-        this.state={
-            firstName: 'My realname',
+        this.state = {formData: {
+            firstName: '',
             lastName: '',
             email: '',
-            password: ''
-        };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.submitFormData = this.submitFormData.bind(this);
+            password: '',
+        }}
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.updateFirstName = this.updateFirstName.bind(this);
+        this.updateLastName = this.updateLastName.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        // this.submitFormData = this.submitFormData.bind(this);
     }
 
-    handleChange(event){
-        this.setState({firstName: event.target.firstName});
+    updateFirstName(event){
+        const formData = this.state.formData
+        formData.firstName = event.target.value
+        this.setState({ formData })
+    }
+
+    updateLastName(event){
+        const formData = this.state.formData
+        formData.lastName = event.target.value
+        this.setState({ formData })
+    }
+
+    updateEmail(event){
+        const formData = this.state.formData
+        formData.email = event.target.value
+        this.setState({ formData })
+    }
+
+    updatePassword(event){
+        const formData = this.state.formData
+        formData.password = event.target.value
+        this.setState({ formData })
     }
 
     handleSubmit(event){
-        alert('a name was submitted: ' + this.state.firstName);
         event.preventDefault();
-    }
+        console.log(this.state);
 
-    submitFormData(){
-        axios.post('/user', {
-        firstName: this.state.firstName
-        }).then(function(response){
-            console.log(response);
-        }).catch(function(error){
-            console.log(error)
-        })
+         axios.post('/api/users', { 
+            user: {
+                first_name: this.state.formData.firstName, 
+                last_name: this.state.formData.lastName,
+                email: this.state.formData.email,
+                password: this.state.formData.password 
+            }
+            }).then(function(response){
+                console.log(response);
+            }).catch(function(error){
+                console.log(error)
+            })
     }
-
+    
     render () {
         return(
         	<div className="Registration" style={{height: '200px', backgroundColor: 'honeydew'}}>
         		<h2>Registration COMPONENT</h2>
-                <form onSubmit={this.submitFormData()}>
-                    <input type="text" value={this.state.firstName} onChange={this.handleChange} placeholder="first name"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.formData.firstName} placeholder="first name" onChange={this.updateFirstName} />
+                    <input type="text" value={this.state.formData.lastName} placeholder="last name" onChange={this.updateLastName} />
+                    <input type="email" value={this.state.formData.email} placeholder="email" onChange={this.updateEmail} />
+                    <input type="password" value={this.state.formData.password} placeholder="password" onChange={this.updatePassword} />
                     <input type="submit"/>
                 </form>
-                <p>{this.state.firstName}</p>
         	</div>
         )
       }
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users 
 
   namespace :api do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :users, only: [:show] do
+    resources :users do
       resources :reviews
-    end
+    end 
 
     resources :conversations do
       resources :messages
