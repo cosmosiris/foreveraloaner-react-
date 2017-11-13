70975ff70e21567d9406aa81ca1db1972e38dee3
Caroline <carolyoon731@gmail.com>
2017-11-13 09:44:08 -0800

    Update AppRouter.js

--- app/javascript/components/AppRouter.js
+++ app/javascript/components/AppRouter.js
@@ -6,24 +6,34 @@ import App from './App'
 import MyProfile from './MyProfile'
 import CreatePost from './CreatePost'
 import Header from './Header'
+import Post from './Post'
 import Footer from './Footer'
+import CategoriesContainer from './CategoriesContainer'
+import Category from './Category'
+import PostsContainer from './PostsContainer'
+import SearchForm from './SearchForm'
+import axios from 'axios'
 import Registration from './Registration'
 
 
 class AppRouter extends React.Component {
+
   render () {
     return(
     	<BrowserRouter>
         <div>
           <Header />
       		<Switch>
-  	            <Route exact path="/" component={App} />
-  	            <Route exact path="/api/user/:id" component={MyProfile} />
-                <Route exact path="/post/new" component={CreatePost} />     
-  	            <Route exact path="/user/new" component={Registration} />     
-  	        </Switch>   
-            <Footer />  
-        </div>      
+            <Route exact path="/" component={App} />
+            <Route path="/categories/:id" component={Category} />
+            <Route path="/posts/:id" component={Post} />
+            <Route exact path="/api/user/:id" component={MyProfile} />
+            <Route exact path="/post/new" component={CreatePost} />
+            <Route exact path="/user/new" component={Registration} />
+  	       </Switch>
+            <SearchForm />
+          <Footer />
+          </div>
     	</BrowserRouter>
     )
   }
