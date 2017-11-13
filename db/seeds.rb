a0e80184fadfa4fa073c894042c28b662bed97ac
Caroline & Shannon Nolan <pair+carolyoon.shannonnolan17@devbootcamp.com>
2017-11-12 16:39:28 -0800

    Add categories as seeds and delete manifest.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,17 @@
 User.destroy_all
 Review.destroy_all
 
-borrower = User.create(first_name: "britt", last_name: "nevins", email:"britt@nevins.com", password: "asdfasdfasdf")
-lender = User.create(first_name: "Olivia", last_name: "Noland", email:"olivia@noland.com", password: "asdfasdfasdf")
+borrower = User.create!(first_name: "britt", last_name: "nevins", email:"britt@nevins.com", password: "asdfasdfasdf")
+lender = User.create!(first_name: "Olivia", last_name: "Noland", email:"olivia@noland.com", password: "asdfasdfasdf")
 
+categories = ["Accessories", "Baby & Kid Products", "Beauty Products", "Books", "Business & Office Supplies", "Clothing", "DIY Supplies", "Electronics", "Farm & Garden", "Fitness & Sporting Equipment", "Furniture", "Games", "Household Items & Appliances", "Kitchen Appliances", "Luggage & Travel Accessories", "Miscellaneous", "Musical Instruments", "Outdoor Equipment", "Party Supplies", "Tools", "Trailers", "Vehicle & Automotive Parts", "VHS & DVD" ]
+
+categories.each do |category|
+  Category.create!(name: category)
+end
 
 Review.create(rating: 3, body: "great", reviewer: borrower, reviewee: lender, role:"borrower")
+
+
 
 
